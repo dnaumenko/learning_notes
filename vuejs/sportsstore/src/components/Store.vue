b1a2033cec882fbbea8f65173838ecdd07ac8a85
dnaumenko <dm.naumenko@gmail.com>
2018-09-30 14:08:29 +0300

    other vuejs code

---
+++ vuejs/sportsstore/src/components/Store.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SPORTS STORE</a>
+                <cart-summary/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <CategoryControls />
+                <button class="btn btn-block btn-warning mt-5"
+                        v-on:click="setShowSearch(true)">
+                    Search
+                </button>
+            </div>
+            <div class="col-9 p-2">
+                <Search />
+                <ProductList />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import ProductList from "./ProductList";
+    import CategoryControls from "./CategoryControls";
+    import CartSummary from "./CartSummary";
+    import Search from "./Search";
+
+    import { mapMutations } from "vuex"; 
+
+    export default {
+        components: { ProductList, CategoryControls, CartSummary, Search },
+        methods: {
+            ...mapMutations(["setShowSearch"])
+        }
+    }
+
+</script>
---
+++ vuejs/sportsstore/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+import store from "./store";
+import router from "./router";
+import Vuelidate from "vuelidate";
+
+Vue.config.productionTip = false;
+
+Vue.filter("currency", (value) => new Intl.NumberFormat("en-US",
+  {style: "currency", currency: "USD"}
+).format(value));
+
+Vue.use(Vuelidate);
+
+new Vue({
+  render: h => h(App),
+  store, // it's important to add store here in config
+  router
+}).$mount('#app');
